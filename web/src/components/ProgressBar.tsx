85ce168b28fb7a0ee68071ac7cc247e779dcf4f4
jcagz96 <jcagz@iscte-iul.pt>
2023-01-21 13:13:22 +0000

    version 1.0

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,23 @@
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar({ progress }: ProgressBarProps) {
+  const progressStyles = {
+    width: `${progress}%`
+  }
+
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        className="h-3 rounded-xl bg-violet-600  transition-all"
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={progress}
+        style={{ width: `${progress}%` }}
+      >
+
+      </div>
+    </div>
+  );
+}
